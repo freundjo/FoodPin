b8aa1512fe0e9af995fa2edf0405eb55a817725e
Jordan Freund <jordanfreund@Jordans-MacBook-Pro.local>
2017-02-05 13:44:58 -0600

    further updates

---
+++ FoodPin/WalkthroughContentViewController.swift
@@ -0,0 +1,73 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 18/8/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+
+    @IBOutlet var headingLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var contentImageView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet var forwardButton: UIButton!
+    
+    var index = 0
+    var heading = ""
+    var imageFile = ""
+    var content = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headingLabel.text = heading
+        contentLabel.text = content
+        contentImageView.image = UIImage(named: imageFile)
+        pageControl.currentPage = index
+        
+        switch index {
+        case 0...1: forwardButton.setTitle("NEXT", for: .normal)
+        case 2: forwardButton.setTitle("DONE", for: .normal)
+        default: break
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - IBAction Methods
+    
+    @IBAction func nextButtonTapped(sender: UIButton) {
+        
+        switch index {
+        case 0...1: // Next Button
+            let pageViewController = parent as! WalkthroughPageViewController
+            pageViewController.forward(index: index)
+            
+        case 2: // Done Button
+            UserDefaults.standard.set(true, forKey: "hasViewedWalkthrough")
+            dismiss(animated: true, completion: nil)
+            
+            // Add app quick actions
+            if traitCollection.forceTouchCapability == .available {
+                let bundleIdentifier = Bundle.main.bundleIdentifier
+                let shortcutItem1 = UIApplicationShortcutItem(type: "\(bundleIdentifier).OpenFavorites", localizedTitle: "Show Favorites", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(templateImageName: "favorite-shortcut"), userInfo: nil)
+                let shortcutItem2 = UIApplicationShortcutItem(type: "\(bundleIdentifier).OpenDiscover", localizedTitle: "Discover restaurants", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(templateImageName: "discover-shortcut"), userInfo: nil)
+                let shortcutItem3 = UIApplicationShortcutItem(type: "\(bundleIdentifier).NewRestaurant", localizedTitle: "New Restaurant", localizedSubtitle: nil, icon: UIApplicationShortcutIcon(type: .add), userInfo: nil)
+                UIApplication.shared.shortcutItems = [shortcutItem1, shortcutItem2, shortcutItem3]
+                
+            }
+            
+        default: break
+            
+        }
+    }
+}
